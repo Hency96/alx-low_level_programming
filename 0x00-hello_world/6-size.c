d53e0f614b2791f58d8c1423ab46a5f8e245589c
Monday <hencynikky@gmail.com>
2023-09-07 16:14:26 -0700

    a program for sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that print different type sizes
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("size of char: %lu bytes(s)\n", (unsigned long) sizeof(a));
+printf("size of int: %lu bytes(s)\n", (unsigned long)  sizeof(b));
+printf("size of long int: %lu bytes(s)\n", (unsigned long)  sizeof(c));
+printf("size of long logn int: %lu bytes(s)\n", (unsigned long)  sizeof(d));
+printf("size of float: %lu bytes(s)\n", (unsigned long)  sizeof(f));
+return (0);
+}
+
