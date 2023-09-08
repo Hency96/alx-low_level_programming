532595234c01be3010d6536c8a15d7485ee98f68
Monday <hencynikky@gmail.com>
2023-09-07 17:16:36 -0700

    unix and c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *  main -a program that prints outs a set of strings
+ * return: 1(succes)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (0);
+}
