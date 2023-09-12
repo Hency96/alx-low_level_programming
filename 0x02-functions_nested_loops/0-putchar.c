f4cd5c56d5afe264a2403772df29aada14b9f8d8
Monday <hencynikky@gmail.com>
2023-09-12 15:31:30 -0700

    fix for putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main - A program that prints a string (_putchar)
- * Return 0;
+ * main - A program in c that print putchar
+ * Return: 0
  */
-main(void)
+
+int main(void)
 {
-	_putchar();
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
 
