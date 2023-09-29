a03760a0660e81c7fa4799565298c1882d6c3947
Monday <hencynikky@gmail.com>
2023-09-28 21:21:17 -0700

    all project

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,10 +7,11 @@
 int _strlen_recursion(char *s)
 {
 	int longit = 0;
+
 	if (*s)
 	{
 		longit++;
-		longit +- _strlen_recursion(s + 1);
+		longit + -_strlen_recursion(s + 1);
 	}
 	return (longit);
 }
