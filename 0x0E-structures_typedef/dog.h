87cd44549f81c99c9e9bbb7d3a61021afa3aa56e
Monday <hencynikky@gmail.com>
2023-10-10 11:30:31 -0700

    struct and def prject

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+
+/**
+ * struct dog - a struct for to hold certain variable of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owners name
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+return (dog);
+};
