16f201bcc345937fd33b06981e413c83af40e1fe
Monday <hencynikky@gmail.com>
2023-10-10 12:43:00 -0700

    initialize variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,8 @@
+#include <dog.h>
+#include <main.h>
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	name = Poppy;
+	age = 3.5;
+	owner = bob;
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,6 @@
+#ifndef INV_TREE_H
+#define INV_TREE_H
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
