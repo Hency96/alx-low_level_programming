ffa8967da3978df1efaf5b445c15bf0d40932e2c
Monday <hencynikky@gmail.com>
2023-10-30 16:03:22 -0700

    fix for append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (nletter = 0; text_content[nletters]; nletter++)
+		for (nletter = 0; text_content[nletter]; nletter++)
 			;
 
 		rw = write(fdr, text_content, nletter);
