7eb91f0ef5c619c7ee4796825bc615b3dc777cdd
Monday <hencynikky@gmail.com>
2024-04-11 01:19:31 -0700

    fixing task 2

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,50 +12,50 @@
  */
 int recursive_search(int *array, size_t size, int value)
 {
-    size_t half = size / 2;
-    size_t i;
+	size_t half = size / 2;
+	size_t i;
 
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    printf("Searching in array");
+	printf("Searching in array");
 
-    for (i = 0; i < size; i++)
-        printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
 
-    printf("\n");
+	printf("\n");
 
-    if (half && size % 2 == 0)
-        half--;
+	if (half && size % 2 == 0)
+		half--;
 
-    if (value == array[half])
-        return ((int)half);
+	if (value == array[half])
+		return ((int)half);
 
-    if (value < array[half])
-        return (recursive_search(array, half, value));
+	if (value < array[half])
+		return (recursive_search(array, half, value));
 
-    half++;
+	half++;
 
-    return (recursive_search(array + half, size - half, value) + half);
+	return (recursive_search(array + half, size - half, value) + half);
 }
 
 /**
  * binary_search - calls to binary_search to return
- * the index of the number
+ * index of a number
  *
  * @array: input array
- * @size: size of an array
+ * @size: size of the array
  * @value: value to search in
  * Return: index of the number
  */
 int binary_search(int *array, size_t size, int value)
 {
-    int i;
+	int index;
 
-    i = recursive_search(array, size, value);
+	index = recursive_search(array, size, value);
 
-    if (i >= 0 && array[i] != value)
-        return (-1);
+	if (index >= 0 && array[index] != value)
+		return (-1);
 
-    return (i;
+	return (index);
 }
